2f4229d47a819d8f7fcb743467a6b81c3f0231bc
Linda <oneill.linda21@gmail.com>
2018-04-12 19:36:46 -0500

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,21 @@
 require 'random_data'
 
+user = User.first
+user.update_attributes!(
+  email: 'oneill.linda21@gmail.com',
+  password: 'helloworld'
+)
+
+5.times do
+  User.create!(
+
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -11,6 +27,7 @@ topics = Topic.all
 50.times do
   Post.create!(
 
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -27,6 +44,7 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
