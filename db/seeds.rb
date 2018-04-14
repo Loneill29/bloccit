d1552e1a6e647b696d36b02d8faf89aef121c6e7
Linda <oneill.linda21@gmail.com>
2018-04-14 15:11:55 -0500

    add admin and member users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,16 @@ require 'random_data'
 end
 users = User.all
 
-user = User.first
-user.update_attributes!(
-  email: 'oneill.linda21@gmail.com',
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
