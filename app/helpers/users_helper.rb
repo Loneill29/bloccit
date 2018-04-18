a8468a850bdf8b099aa9e60f709a05dd0d0f5c98
Linda <oneill.linda21@gmail.com>
2018-04-18 10:45:59 -0500

    move logic to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,19 @@
 module UsersHelper
   def user_has_comments?
-  @user.comments.any?
-end
+    if  @user.comments.any?
+      @user.comments
+    else "#{@user.name} has not written any comments."
+    end
+  end
 
-def user_has_posts?
-  @user.posts.any?
-end
+  def user_has_posts?
+    if @user.posts.any?
+      @user.posts
+    else "#{@user.name} has not written any posts."
+    end
+  end
 
-def user_has_favorites?
-  @user.favorites.any?
-end
+  def user_has_favorites?
+    @user.favorites.any?
+  end
 end
